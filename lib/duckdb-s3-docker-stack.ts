1251d379215a30782ec812b902475f79d95f0bf4
Eduardo Ramón <edu.rz@hotmail.com>
2024-10-23 10:17:19 -0500

    Initial commit

---
+++ lib/duckdb-s3-docker-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DuckdbS3DockerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'DuckdbS3DockerQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/duckdb-s3-docker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DuckdbS3Docker from '../lib/duckdb-s3-docker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/duckdb-s3-docker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DuckdbS3Docker.DuckdbS3DockerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
